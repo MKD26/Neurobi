c1ddaf7ef88adae37a692a3e344dc348242ee80c
Nilski10 <nils@cj-kittler.de>
2022-05-04 10:42:42 +0200

    Projektdatei

---
+++ Raumschiff/Assets/RobotSphere/Assets/Scripts/RobotFreeAnim.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotFreeAnim : MonoBehaviour {
+
+	Vector3 rot = Vector3.zero;
+	float rotSpeed = 40f;
+	public float speedvor = 5f; 
+
+	Animator anim;
+
+	// Use this for initialization
+	void Awake()
+	{
+		anim = gameObject.GetComponent<Animator>();
+		gameObject.transform.eulerAngles = rot;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		CheckKey();
+		gameObject.transform.eulerAngles = rot;
+	}
+
+	void CheckKey()
+	{
+		// Walk
+		if (Input.GetKey(KeyCode.W))
+		{
+			anim.SetBool("Walk_Anim", true);
+			transform.Translate(Vector3.forward * Time.deltaTime * speedvor);
+		}
+		else if (Input.GetKeyUp(KeyCode.W))
+		{
+			anim.SetBool("Walk_Anim", false);
+			transform.Translate(Vector3.up * Time.deltaTime * speedvor);
+		}
+
+		// Rotate Left
+		if (Input.GetKey(KeyCode.A))
+		{
+			rot[1] -= rotSpeed * Time.fixedDeltaTime;
+		}
+
+		// Rotate Right
+		if (Input.GetKey(KeyCode.D))
+		{
+			rot[1] += rotSpeed * Time.fixedDeltaTime;
+		}
+
+		// Roll
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			if (anim.GetBool("Roll_Anim"))
+			{
+				anim.SetBool("Roll_Anim", false);
+			}
+			else
+			{
+				anim.SetBool("Roll_Anim", true);
+			}
+		}
+
+		// Close
+		if (Input.GetKeyDown(KeyCode.LeftControl))
+		{
+			if (!anim.GetBool("Open_Anim"))
+			{
+				anim.SetBool("Open_Anim", true);
+			}
+			else
+			{
+				anim.SetBool("Open_Anim", false);
+			}
+		}
+	}
+
+}
